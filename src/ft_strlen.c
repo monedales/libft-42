debfd8a4365762e670c461d8ef601e79c6247f9b
monedales <le.moneda@outlook.com>
2025-07-14 18:42:17 -0300

    :sparkles: feat: adding ft_strlen function

---
+++ src/ft_strlen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maria-ol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 16:37:26 by maria-ol          #+#    #+#             */
+/*   Updated: 2025/07/14 18:14:27 by maria-ol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+/**
+ * @brief Computes the length of a null-terminated string.
+ *
+ * Counts the number of characters in the string until the null terminator.
+ *
+ * @param str The string whose length will be calculated.
+ * @return The number of characters in the string, excluding the null terminator.
+ */
+
+size_t	ft_strlen(char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
