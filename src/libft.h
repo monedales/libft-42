c4edd4bc5693f8e77df8a1f9ecbc690a718879b2
monedales <le.moneda@outlook.com>
2025-08-01 18:51:43 -0300

    ✨ adding ft_lstmap bonus function

--- src/libft.h
+++ src/libft.h
@@ -6,7 +6,7 @@
 /*   By: maria-ol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 20:13:11 by maria-ol          #+#    #+#             */
-/*   Updated: 2025/08/01 13:53:25 by maria-ol         ###   ########.fr       */
+/*   Updated: 2025/08/01 18:50:51 by maria-ol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <limits.h>
+# include <string.h>
+
 
 typedef struct s_list
 {
@@ -64,5 +66,9 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+void	del_content(void *content);
 
 #endif
